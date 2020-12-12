d4014eb844c2b9538ba3e2ba006d5805c911e9a2
SubbotaAnton <subbotaanton@gmail.com>
2020-12-12 18:43:55 +0200

    Finishing first page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 (function (global) {
 
+    document.getElementById('submitForm').addEventListener('click', onSubmit, false);
+
+    function onSubmit(event) {
+        event.preventDefault();
+
+        const checkedRadioButton = document.querySelector('.businessFormList input[type="radio"]:checked')
+
+        console.log(checkedRadioButton.value);
+    }
+
 }(window));
